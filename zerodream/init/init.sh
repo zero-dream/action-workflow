0e23096983ccec7912566786b5a0b6133fd163a9
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-01-17 21:08:00 +0800

    1

--- zerodream/init/init.sh
+++ zerodream/init/init.sh
@@ -33,6 +33,7 @@ setEnv 'CI_StoragePath' "$GITHUB_WORKSPACE/storage"
 # --------------------------------------------------
 
 # ZeroDreamCore
+source "$scriptDir/script/initEnv.sh"
 source "$scriptDir/script/mergeRepo.sh"
 source "$scriptDir/script/check.sh"
 source "$scriptDir/script/envVar.sh"
