0d271ec6d4d6d142a5ba5cced971941e1594af98
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-03-04 18:30:04 +0800

    Initial repository

---
+++ zerodream/init/script/initEnv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (C) 2000 ZeroDream
+# https://github.com/zero-dream
+
+# --------------------------------------------------
+
+# Npm
+sudo npm install -g json5 &>/dev/null
+
+# --------------------------------------------------
+
+exit 0
